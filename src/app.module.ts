bdb89745f3d64a16d4f2ec03d97469fcd366a717
bratik121 <thiswasbrii@gmail.com>
2023-05-01 21:17:47 -0400

    craando modulos, servicios y controlladores para las tareas y usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TaskController } from './task/task.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, TasksModule],
+  controllers: [AppController, TaskController],
   providers: [AppService],
 })
 export class AppModule {}
