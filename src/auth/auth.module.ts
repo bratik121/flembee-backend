0de5e41a1ffb5483dc0f49a7fdab797813a59ce1
bratik121 <thiswasbrii@gmail.com>
2023-05-02 10:53:28 -0400

    generado token en base al usuario

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/config/db.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { User } from 'src/entities/user.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), DatabaseModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    DatabaseModule,
+    JwtModule.register({
+      secret: 'secret', //process.env.SECRET,
+      signOptions: { expiresIn: '12h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,16 +4,19 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { hash, compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private authRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   async register(registerAuthDto: RegisterAuthDto) {
     const { password } = registerAuthDto; //contraseña plana
     const plainToHash = await hash(password, 10); //contraseña encriptada
+    //registro el usuario en la bases de datos
     return this.authRepository.save({
       ...registerAuthDto,
       password: plainToHash,
@@ -21,18 +24,28 @@ export class AuthService {
   }
 
   async login(loginAuthDto: LoginAuthDto) {
+    //busco el usuario en la base de datos
     const findUSer = await this.authRepository.findOne({
       where: { username: loginAuthDto.username },
     });
-
+    //si no lo encuentro retorno un error
     if (!findUSer) {
       return { code: 404, message: 'Usuario no encontrado' };
     } else {
+      //si lo encuentro comparo las contraseñas
       const isMatch = await compare(loginAuthDto.password, findUSer.password);
+      //si no coinciden retorno un error
       if (!isMatch) {
         return { code: 400, message: 'Contraseña incorrecta' };
       }
-      return { code: 200, message: 'Usuario logueado' };
+      //si coinciden retorno el token y el usuario
+      const payload = { usernae: findUSer.username };
+      const token = await this.jwtService.signAsync(payload);
+      const data = {
+        username: findUSer.username,
+        token,
+      };
+      return { code: 200, data };
     }
   }
 }
